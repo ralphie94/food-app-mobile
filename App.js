a54171bf1d6c652a0638963f6e6950928c9fe3fb
Ralphie <ralphie94@pacbell.net>
2020-08-16 23:23:54 -0700

    started react native app, started search bar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import SearchScreen from "./src/screens/SearchScreen";
+import SearchScreen from "./src/components/screens/SearchScreen";
 
 const navigator = createStackNavigator({
   Search: SearchScreen
